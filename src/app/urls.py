b60fc35a0170cc7e7db7e1815c33491dc4e7865e
yurivfernandes1 <yurivf@outlook.com>
2026-02-07 21:14:03 -0300

    fix: Corrigir erro de sintaxe no arquivo de URLs

--- src/app/urls.py
+++ src/app/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.static import serve
+
 from .health import health
 
 urlpatterns = [
@@ -33,4 +34,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
     )
